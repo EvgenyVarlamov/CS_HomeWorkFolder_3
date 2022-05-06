b262591bbdda3c46aae003799308431cc4ac07a5
EvgenyVarlamov <varlamov-el@yandex.ru>
2022-05-06 22:59:30 +0300

    add Palindrom 2nd variant

--- Homework_1/Program.cs
+++ Homework_1/Program.cs
@@ -3,7 +3,20 @@
 23432 -> да
 12821 -> да*/
 
+// Первый вариант
+/*
 Console.WriteLine($"Введите 5-значное число");
-string? number = Console.ReadLine();
+string? number = String.Empty;
+number = Console.ReadLine();
 if(number[0] == number[4] && number[1] == number[3]) Console.WriteLine($"Введенное чиило {number} является палиндромом");
+else Console.WriteLine($"Введенное чиило {number} не является палиндромом");
+*/
+// Второй вариант
+
+Console.WriteLine($"Введите 5-значное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number % 10 == number % 100000 / 10000 && number % 100 / 10 == number % 10000 / 1000)
+{
+    Console.WriteLine($"Введенное чиило {number} является палиндромом");
+}
 else Console.WriteLine($"Введенное чиило {number} не является палиндромом");
